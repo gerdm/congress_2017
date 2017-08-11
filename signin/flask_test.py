bd3bef48a82cdf3c1a9101659b1a85b6eecce16a
ger <g.duran@me.com>
2017-08-11 08:17:44 -0500

    add test files; modified test python file

--- signin/flask_test.py
+++ signin/flask_test.py
@@ -1,8 +1,10 @@
-from flask import Flask
-from flask import redirect
+from flask import Flask, redirect, render_template
+from flask_script import Manager
 
 # Tell Flask where to look the 'working files'
 app = Flask(__name__)
+# Provides CLI properties when debugging the webapp
+manager = Manager(app)
 
 # This tells flask what to run once a get request is
 # sent to the server. In this case, it is the 'main'
@@ -10,11 +12,11 @@ app = Flask(__name__)
 # part of the code
 @app.route("/")
 def index():
-    return "<h1>Hello, world!</h1>"
+    return render_template("index.html")
 
 @app.route("/user/<name>")
 def user(name):
-    return "<h1> Hello, {name}</h1>".format(name=name)
+    return render_template("user.html", name=name)
 
 # The 'redirect' method allows the webpage to take you
 # to another page and return a 302 response: redirect
@@ -23,4 +25,6 @@ def goto_github():
     return redirect("https://github.com/gerdm")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #Replaces 'app.run(debug=True)' in order 
+    # to give CLI functionalities (see manager)
+    manager.run()
