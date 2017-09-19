ae0ee0f42b160cb3ba371aef98a85bbc0e01b828
ger <g.duran@me.com>
2017-09-19 10:08:37 -0500

    edit forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,11 +8,11 @@ class MembersForm(FlaskForm):
     last_name_mother = StringField("Apellido Materno", validators=[Required()])
     email = StringField("Email", validators=[Required()])
     school = StringField("Escuela", validators=[Required()])
-    beverage = SelectField("Bebida", coerce=int)
     # Dinamyc creation of the form for grade and workshop: setting the data afterwards
     grade = SelectField("Grado de Estudio", coerce=int, id="select_grade")
     round_table = SelectField("Selecciona la mesa rendonda a atender", coerce=int, id="select_table")
     workshop = SelectField("Selecciona el taller a participar", coerce=int, id="select_workshop")
+    beverage = SelectField("Bebida", coerce=int)
     secret_code = StringField("Validación de código", validators=[Required()])
     submit = SubmitField("Enviar")
 
